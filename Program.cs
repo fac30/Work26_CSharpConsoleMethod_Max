06307706984da477db4466728622d39a2594929a
maxitect <maxime.downe@gmail.com>
2024-11-08 16:29:40 +0000

    feat: exercise on returning integers using currency conversion methods module 3

--- Program.cs
+++ Program.cs
@@ -1,29 +1,17 @@
-﻿double total = 0;
-double minimumSpend = 30.00;
+﻿double usd = 23.73;
+int vnd = UsdToVnd(usd);
 
-double[] items = {15.97, 3.50, 12.25, 22.99, 10.98};
-double[] discounts = {0.30, 0.00, 0.10, 0.20, 0.50};
+Console.WriteLine($"${usd} USD = ${vnd} VND");
+Console.WriteLine($"${vnd} VND = ${VndToUsd(vnd)} USD");
 
-for (int i = 0; i < items.Length; i++)
+int UsdToVnd(double usd) 
 {
-    total += GetDiscountedPrice(i);
+    int rate = 23500;
+    return (int) (rate * usd);
 }
 
-total -= TotalMeetsMinimum() ? 5.00 : 0.00;
-
-Console.WriteLine($"Total: ${FormatDecimal(total)}");
-
-double GetDiscountedPrice(int itemIndex)
+double VndToUsd(int vnd) 
 {
-    return items[itemIndex] * (1 - discounts[itemIndex]);
-}
-
-bool TotalMeetsMinimum()
-{
-    return total >= minimumSpend;
-}
-
-string FormatDecimal(double input)
-{
-    return input.ToString().Substring(0, 5);
+    double rate = 23500;
+    return vnd / rate;
 }
