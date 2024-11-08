417f2d15e5579de050b0a6d929d9b2660e0b79e0
maxitect <maxime.downe@gmail.com>
2024-11-08 16:43:20 +0000

    feat: exercise on returning arrays using change finding method module 3

--- Program.cs
+++ Program.cs
@@ -1,25 +1,44 @@
-﻿string[] words = ["racecar", "talented", "deified", "tent", "tenet"];
+﻿int target = 30;
+int[] coins = new int[] {5, 5, 50, 25, 25, 10, 5};
+int[,] result = TwoCoins(coins, target);
 
-Console.WriteLine("Is it a palindrome?");
-foreach (string word in words)
+if (result.Length == 0) 
 {
-    Console.WriteLine($"{word}: {IsPalindrome(word)}");
+    Console.WriteLine("No two coins make change");
+} 
+else 
+{
+    Console.WriteLine("Change found at positions:");
+    for (int i = 0; i < result.GetLength(0); i++) 
+    {
+        if (result[i,0] == -1) 
+        {
+            break;
+        }
+        Console.WriteLine($"{result[i,0]},{result[i,1]}");
+    }
 }
 
-bool IsPalindrome(string word)
+int[,] TwoCoins(int[] coins, int target) 
 {
-    int start = 0;
-    int end = word.Length - 1;
+    int[,] result = {{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1}};
+    int count = 0;
 
-    while (start < end)
+    for (int curr = 0; curr < coins.Length; curr++) 
     {
-        if (word[start] != word[end])
-        {
-            return false;
+        for (int next = curr + 1; next < coins.Length; next++) 
+        {    
+            if (coins[curr] + coins[next] == target) 
+            {
+                result[count, 0] = curr;
+                result[count, 1] = next;
+                count++;
+            }
+            if (count == result.GetLength(0)) 
+            {
+                return result;
+            }
         }
-        start++;
-        end--;
     }
-
-    return true;
+    return (count == 0) ? new int[0,0] : result;
 }
