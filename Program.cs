99128c20e028f1d05c202f29df93cefeed055960
maxitect <maxime.downe@gmail.com>
2024-11-08 14:52:18 +0000

    build: initial dotnet console application build and module 1 random number method

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Generating random numbers:");
+DisplayRandomNumbers();
+
+static void DisplayRandomNumbers() 
+{
+    Random random = new();
+
+    for (int i = 0; i < 5; i++) 
+    {
+        Console.Write($"{random.Next(1, 100)} ");
+    }
+
+    Console.WriteLine();
+}
