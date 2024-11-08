8160d29898543abb47aa632b66fc86b3ded94cd7
maxitect <maxime.downe@gmail.com>
2024-11-08 15:30:15 +0000

    feat: challenge to create tellFortune method module 1

--- Program.cs
+++ Program.cs
@@ -1,56 +1,19 @@
-﻿string[] ipv4Input = ["107.31.1.5", "255.0.0.255", "555..0.555", "255...255"];
-string[] address;
-bool validLength = false;
-bool validZeroes = false;
-bool validRange = false;
+﻿Random random = new();
+int luck = random.Next(100);
 
-foreach (string ip in ipv4Input)
+string[] text = ["You have much to", "Today is a day to", "Whatever work you do", "This is an ideal time to"];
+string[] good = ["look forward to.", "try new things!", "is likely to succeed.", "accomplish your dreams!"];
+string[] bad = ["fear.", "avoid major decisions.", "may have unexpected outcomes.", "re-evaluate your life."];
+string[] neutral = ["appreciate.", "enjoy time with friends.", "should align with your values.", "get in tune with nature."];
+
+Console.WriteLine("A fortune teller whispers the following words:");
+tellFortune(luck);
+
+void tellFortune(int luck)
 {
-    address = ip.Split(".", StringSplitOptions.RemoveEmptyEntries);
-
-    ValidateLength();
-    ValidateZeroes();
-    ValidateRange();
-
-    if (validLength && validZeroes && validRange)
+    string[] fortune = (luck > 75 ? good : (luck < 25 ? bad : neutral));
+    for (int i = 0; i < 4; i++)
     {
-        Console.WriteLine($"{ip} is a valid IPv4 address");
+        Console.Write($"{text[i]} {fortune[i]} ");
     }
-    else
-    {
-        Console.WriteLine($"{ip} is an invalid IPv4 address");
-    }
-}
-
-void ValidateLength()
-{
-    validLength = address.Length == 4;
-};
-
-void ValidateZeroes()
-{
-    foreach (string number in address)
-    {
-        if (number.Length > 1 && number.StartsWith("0"))
-        {
-            validZeroes = false;
-            return;
-        }
-    }
-
-    validZeroes = true;
-}
-
-void ValidateRange()
-{
-    foreach (string number in address)
-    {
-        int value = int.Parse(number);
-        if (value < 0 || value > 255)
-        {
-            validRange = false;
-            return;
-        }
-    }
-    validRange = true;
 }
