9ac5faa70edacfa59e6c6df166f4e78749ad3b8d
maxitect <maxime.downe@gmail.com>
2024-11-08 15:53:23 +0000

    feat: exercise on calculating and printing circle area and circumference methods module 2

--- Program.cs
+++ Program.cs
@@ -1,25 +1,24 @@
-﻿int[] schedule = [800, 1200, 1600, 2000];
+﻿double pi = 3.14159;
+PrintCircleInfo(12);
+PrintCircleInfo(24);
 
-DisplayAdjustedTimes(schedule, 6, -6);
-
-void DisplayAdjustedTimes(int[] times, int currentGMT, int newGMT)
+void PrintCircleArea(int radius)
 {
-    int diff = 0;
-    if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
-    {
-        Console.WriteLine("Invalid GMT");
-    }
-    else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
-    {
-        diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
-    }
-    else
-    {
-        diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
-    }
-    for (int i = 0; i < times.Length; i++)
-    {
-        int newTime = ((times[i] + diff)) % 2400;
-        Console.WriteLine($"{times[i]} -> {newTime}");
-    }
+    double pi = 3.14159;
+    double area = pi * (radius * radius);
+    Console.WriteLine($"Area = {area}");
+}
+
+void PrintCircleCircumference(int radius)
+{
+    double pi = 3.14159;
+    double circumference = 2 * pi * radius;
+    Console.WriteLine($"Circumference = {circumference}");
+}
+
+void PrintCircleInfo(int radius)
+{
+    Console.WriteLine($"Circle with radius {radius}");
+    PrintCircleArea(radius);
+    PrintCircleCircumference(radius);
 }
