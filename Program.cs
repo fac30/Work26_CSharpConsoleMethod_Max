a46bee29c9762e3f4b28aa44ca2f6ea7afbe924c
maxitect <maxime.downe@gmail.com>
2024-11-08 16:13:38 +0000

    feat: challenge method printing email addresses for corporate and external personnel module 2

--- Program.cs
+++ Program.cs
@@ -1,44 +1,28 @@
-﻿string[] guestList = { "Rebecca", "Nadia", "Noor", "Jonte" };
-string[] rsvps = new string[10];
-int count = 0;
-
-RSVP(name: "Rebecca", partySize: 1, allergies: "none", inviteOnly: true);
-RSVP(name: "Nadia", partySize: 2, allergies: "Nuts", inviteOnly: true);
-RSVP(name: "Linh", partySize: 2, allergies: "none", inviteOnly: false);
-RSVP(name: "Tony", partySize: 1, allergies: "Jackfruit", inviteOnly: true);
-RSVP(name: "Noor", partySize: 4, allergies: "none", inviteOnly: false);
-RSVP(name: "Jonte", partySize: 2, allergies: "Stone fruit", inviteOnly: false);
-ShowRSVPs();
-
-void RSVP(string name, int partySize, string allergies, bool inviteOnly)
+﻿string[,] corporate =
 {
-    if (inviteOnly)
-    {
-        bool found = false;
-        foreach (string guest in guestList)
-        {
-            if (guest.Equals(name))
-            {
-                found = true;
-                break;
-            }
-        }
-        if (!found)
-        {
-            Console.WriteLine($"Sorry, {name} is not on the guest list");
-            return;
-        }
-    }
+    {"Robert", "Bavin"}, {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+};
 
-    rsvps[count] = $"Name: {name}, \tParty Size: {partySize}, \tAllergies: {allergies}";
-    count++;
-}
-
-void ShowRSVPs()
+string[,] external =
 {
-    Console.WriteLine("\nTotal RSVPs:");
-    for (int i = 0; i < count; i++)
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+
+string corporateDomain = "contoso.com";
+string externalDomain = "hayworth.com";
+
+displayEmails(corporate, corporateDomain);
+displayEmails(external, externalDomain);
+
+void displayEmails(string[,] names, string domain)
+{
+    for (int i = 0; i < names.GetLength(0); i++)
     {
-        Console.WriteLine(rsvps[i]);
+        string firstName = names[i, 0].Substring(0, 2).ToLower();
+        string lastName = names[i, 1].ToLower();
+        Console.WriteLine(firstName + lastName + domain);
     }
 }
