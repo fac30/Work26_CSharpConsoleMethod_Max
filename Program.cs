dff922da5cf86497b286d4ea9754b15cfcaed4b8
maxitect <maxime.downe@gmail.com>
2024-11-08 16:04:23 +0000

    feat: exercise on named arguments creating party RSVP methods module 2

--- Program.cs
+++ Program.cs
@@ -1,67 +1,44 @@
-﻿// Test by value
+﻿string[] guestList = { "Rebecca", "Nadia", "Noor", "Jonte" };
+string[] rsvps = new string[10];
+int count = 0;
 
-int a = 3;
-int b = 4;
-int c = 0;
+RSVP(name: "Rebecca", partySize: 1, allergies: "none", inviteOnly: true);
+RSVP(name: "Nadia", partySize: 2, allergies: "Nuts", inviteOnly: true);
+RSVP(name: "Linh", partySize: 2, allergies: "none", inviteOnly: false);
+RSVP(name: "Tony", partySize: 1, allergies: "Jackfruit", inviteOnly: true);
+RSVP(name: "Noor", partySize: 4, allergies: "none", inviteOnly: false);
+RSVP(name: "Jonte", partySize: 2, allergies: "Stone fruit", inviteOnly: false);
+ShowRSVPs();
 
-Multiply(a, b, c);
-Console.WriteLine($"global statement: {a} x {b} = {c}");
-
-void Multiply(int a, int b, int c) 
+void RSVP(string name, int partySize, string allergies, bool inviteOnly)
 {
-    c = a * b;
-    Console.WriteLine($"inside Multiply method: {a} x {b} = {c}");
-}
-
-// Test by reference
-
-int[] array = {1, 2, 3, 4, 5};
-
-PrintArray(array);
-Clear(array);
-PrintArray(array);
-
-void PrintArray(int[] array) 
-{
-    foreach (int a in array) 
+    if (inviteOnly)
     {
-        Console.Write($"{a} ");
+        bool found = false;
+        foreach (string guest in guestList)
+        {
+            if (guest.Equals(name))
+            {
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+        {
+            Console.WriteLine($"Sorry, {name} is not on the guest list");
+            return;
+        }
     }
-    Console.WriteLine();
+
+    rsvps[count] = $"Name: {name}, \tParty Size: {partySize}, \tAllergies: {allergies}";
+    count++;
 }
 
-void Clear(int[] array) 
+void ShowRSVPs()
 {
-    for (int i = 0; i < array.Length; i++) 
+    Console.WriteLine("\nTotal RSVPs:");
+    for (int i = 0; i < count; i++)
     {
-        array[i] = 0;
+        Console.WriteLine(rsvps[i]);
     }
-}
-
-// Test with strings
-
-// with status as parameter
-string status = "Healthy";
-
-Console.WriteLine($"Start: {status}");
-SetHealth(status, false);
-Console.WriteLine($"End: {status}");
-
-void SetHealth(string status, bool isHealthy) 
-{
-    status = (isHealthy ? "Healthy" : "Unhealthy");
-    Console.WriteLine($"Middle: {status}");
-}
-
-// without status as parameter
-string status = "Healthy";
-
-Console.WriteLine($"Start: {status}");
-SetHealth(false);
-Console.WriteLine($"End: {status}");
-
-void SetHealth(bool isHealthy) 
-{
-    status = (isHealthy ? "Healthy" : "Unhealthy");
-    Console.WriteLine($"Middle: {status}");
 }
